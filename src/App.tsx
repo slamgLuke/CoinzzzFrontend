7219ffa733f73c2cdb07d958a83ec5f1c21c0738
Adrian Cespedes <adrianj.cespedes@hotmail.com>
2024-05-26 15:19:47 -0500

    restructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import { Dashboard } from './Dashboard';
+import { Portfolio } from './Portfolio';
+import { Settings } from './Settings';
 import { Login, Register } from './Auth';
 import './App.css'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
-      <Route index element={<Dashboard />} />
+      {/* <Route path="home" index element={<Dashboard />} > */}
+      <Route path="home" element={<Dashboard />} >
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
       <Route path="login" element={<Login />} />
       <Route path="register" element={<Register />} />
     </Route>
---
+++ src/Portfolio.tsx
@@ -0,0 +1,7 @@
+export function Portfolio() {
+    return (
+        <div>a</div>
+    )
+}
+
+export default Portfolio
