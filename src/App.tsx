8fb1abf389f0ffafaedbd6a3aeb979dbcb676191
Adrian Cespedes <adrianj.cespedes@hotmail.com>
2024-05-26 12:22:38 -0500

    reboot

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import { Box, CssBaseline } from '@mui/material';
-import Sidebar from './components/Sidebar';
-import Topbar from './components/Topbar';
-import CoinTable from './components/CoinTable';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App: React.FC = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <Sidebar />
-      <Box component="main" sx={{ flexGrow: 1, ml: 20 }}>
-        <Topbar />
-        <CoinTable />
-        {/* Agrega más componentes aquí */}
-      </Box>
-    </Box>
-  );
-};
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
