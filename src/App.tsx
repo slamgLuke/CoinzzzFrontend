3c03eedbad7a0ccc4df78a9dceb8d8e3ed9b6f76
slamgLuke <lucascarranzab2@gmail.com>
2024-05-26 14:09:12 -0500

    gaming

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import { Dashboard } from './Dashboard';
+import { Login } from './Login';
 import './App.css'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Dashboard />}>
+    <Route path="/">
       <Route index element={<Dashboard />} />
-      {/* <Route path="login" element={<Login />} />
-      <Route path="register" element={<Register />} /> */}
+      <Route path="login" element={<Login />} />
+      {/*<Route path="register" element={<Register />} />*/}
     </Route>
   )
 )
