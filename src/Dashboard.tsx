3bbe535d9f09fb325f1b9cc7b5fb770e42f1d17f
Adrian Cespedes <adrianj.cespedes@hotmail.com>
2024-05-26 19:49:10 -0500

    progress

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,26 +1,102 @@
+import { useState } from 'react';
+import DashboardTable from "./DashBoardTable"
+import FollowingListTable from "./FollowingListTable"
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/components/ui/tabs"
 import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import './table.css';
+import { Plus, Search } from 'lucide-react';
 
-export function Dashboard() {
+function FollowCoinMenu() {
     return (
-        <div className="flex flex-col h-full">
-            <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-                <div className="flex items-center">
-                    <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
-                </div>
-                <div
-                    className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1"
-                >
-                    <div className="flex flex-col items-center gap-1 text-center">
-                        <h3 className="text-2xl font-bold tracking-tight">
-                            You have no products
-                        </h3>
-                        <p className="text-sm text-muted-foreground">
-                            You can start selling as soon as you add a product.
-                        </p>
-                        <Button className="mt-4">Add Product</Button>
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="ghost">
+                    <Plus strokeWidth={3} />
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <form className="w-full">
+                        <div className="relative w-full">
+                            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                            <Input
+                                type="search"
+                                placeholder="Search coins..."
+                                className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-2/3"
+                            />
+                        </div>
+                    </form>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="name" className="text-right">
+                            Name
+                        </Label>
+                        <Input
+                            id="name"
+                            defaultValue="Pedro Duarte"
+                            className="col-span-3"
+                        />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="username" className="text-right">
+                            Username
+                        </Label>
+                        <Input
+                            id="username"
+                            defaultValue="@peduarte"
+                            className="col-span-3"
+                        />
                     </div>
                 </div>
-            </main>
+                <DialogFooter>
+                    <Button type="submit">Save changes</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+
+export function Dashboard() {
+    const [activeTab, setActiveTab] = useState("monedas"); // Estado para la pestaña activa
+    return (
+        <div className="flex flex-col h-full">
+            <Tabs defaultValue={activeTab} onValueChange={setActiveTab} className="pt-6 px-6">
+                <div className="flex flex-row items-center">
+                    <TabsList className="grid w-full grid-cols-2 w-[400px]">
+                        <TabsTrigger value="monedas">Monedas</TabsTrigger>
+                        <TabsTrigger value="seguimiento">Lista de seguimiento</TabsTrigger>
+                    </TabsList>
+                    {activeTab === "seguimiento" && (
+                        <div className="ml-auto pr-4">
+                            <FollowCoinMenu />
+                        </div>
+                    )}
+                </div>
+                <TabsContent value="monedas">
+                    <DashboardTable />
+                </TabsContent>
+                <TabsContent value="seguimiento">
+                    <FollowingListTable />
+                </TabsContent>
+            </Tabs>
         </div>
     )
 }
---
+++ src/FollowingListTable.tsx
@@ -0,0 +1,60 @@
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+
+const follow_data = [
+    {
+        name: 'BTC',
+        marketCap: '20B',
+        price: '69,523',
+        today: '+2.12%',
+        week: '+8.21%',
+        favorite: false,
+    },
+    {
+        name: 'ETH',
+        marketCap: '15B',
+        price: '4,123',
+        today: '+1.5%',
+        week: '+5.4%',
+        favorite: true,
+    }
+];
+
+export default function FollowingListTable() {
+    return (
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Name</TableHead>
+                    <TableHead className="hidden sm:table-cell">MarketCap</TableHead>
+                    <TableHead className="text-center">Price</TableHead>
+                    <TableHead className="hidden sm:table-cell">Today</TableHead>
+                    <TableHead className="hidden sm:table-cell">7D</TableHead>
+                    <TableHead className="hidden sm:table-cell">30D</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {follow_data.map((item, index) => (
+                    <TableRow key={index}>
+                        <TableCell>
+                            <div className="font-medium">{item.name}</div>
+                        </TableCell>
+                        <TableCell className="hidden sm:table-cell">{item.marketCap}</TableCell>
+                        <TableCell className="text-center">{item.price}</TableCell>
+                        <TableCell className="hidden md:table-cell">{item.today}</TableCell>
+                        <TableCell className="hidden md:table-cell">{item.week}</TableCell>
+                        <TableCell className="hidden sm:table-cell">
+                            TEMP
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    )
+}
