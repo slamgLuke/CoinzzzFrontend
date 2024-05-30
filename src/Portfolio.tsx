48599ebb94d754efd4e86ee71bfeba127c5c3b83
Adrian-Cespedes <adrianj.cespedes@gmail.com>
2024-05-29 20:25:21 -0500

    fixed portfolio table

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -7,73 +7,116 @@ import { useEffect, useState } from "react";
 import { useUser } from "./UserContext";
 
 export function Portfolio() {
-  const { userId } = useUser();
-  console.log("userId", userId);
-  const [portfolio, setPortfolio] = useState<{
-    networth?: number;
-    transactions?: any[];
-  }>({});
-  const [networth, setNetworth] = useState<number | undefined>(undefined);
+	const { userId } = useUser();
+	console.log("userId", userId);
+	const [portfolio, setPortfolio] = useState<{
+		networth?: number;
+		transactions?: any[];
+	}>({});
+	const [networth, setNetworth] = useState<number | undefined>(undefined);
+	const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    console.log("fetching data");
-    const fetchPortfolio = async () => {
-      const data = await fetch("/TestUsers2.json").then((response) =>
-        response.json(),
-      );
-      const user = data.find((user: { _id: string }) => user._id === userId);
-      if (user) {
-        setPortfolio(user.portfolio);
-      } else {
-        setPortfolio({});
-      }
-    };
+	useEffect(() => {
+		console.log("fetching data");
+		// const fetchPortfolio = async () => {
+		//   try {
+		//     const data = await fetch("/TestUsers2.json").then((response) =>
+		//       response.json(),
+		//     );
+		//     const user = data.find((user: { _id: string }) => user._id === userId);
+		//     if (user) {
+		//       setPortfolio(user.portfolio);
+		//     } else {
+		//       setPortfolio({});
+		//     }
+		//
+		//     if (userId) {
+		//       fetchPortfolio();
+		//     }
+		//
+		//   } catch (error) {
+		//     console.error("Error fetching portfolio data", error);
+		//   } finally {
+		//     setLoading(false);
+		//   }
+		// }, [userId]);
 
-    if (userId) {
-      fetchPortfolio();
-    }
-  }, [userId]);
+		const fetchPortfolio = async () => {
+			try {
+				const response = await fetch("/TestUsers2.json");
+				if (!response.ok) {
+					throw new Error(`HTTP error! status: ${response.status}`);
+				}
+				const data = await response.json();
+				console.log("Fetched data:", data);
+				const user = data.find((user) => user._id === userId);
+				if (user) {
+					setPortfolio(user.portfolio);
+				} else {
+					setPortfolio({}); // No user found, set portfolio to null
+				}
+			} catch (error) {
+				console.error("Failed to fetch portfolio data:", error);
+			} finally {
+				setLoading(false); // Stop loading after data is fetched
+			}
+		};
 
-  useEffect(() => {
-    if (portfolio && portfolio.networth !== undefined) {
-      setNetworth(portfolio.networth);
-    } else {
-      setNetworth(undefined);
-    }
-  }, [portfolio]);
+		fetchPortfolio();
+	}, [userId]);
 
-  console.log(portfolio);
+	if (portfolio === undefined) {
+		console.log("useEffect not working");
+	}
 
-  return (
-    <div>
-      <div className="flex flex1 flex-col align-top">
-        <h1 className="text-4xl font-semibold w-auto text-left py-4 pl-6">
-          Portfolio
-        </h1>
-      </div>
-      <div>
-        <div className="flex flex-col items-center justify-center pb-16">
-          <Card className="items-center justify-center px-16 py-4">
-            <CardDescription>Your Net</CardDescription>
-            <CardTitle className="text-4xl">{networth}</CardTitle>
-            <CardTitle className="text-2xl">0.13 BTC</CardTitle>
-            <div className="text-xs text-muted-foreground">+25%</div>
-            <img src={plot} alt="plot" className="hidden" />
-          </Card>
-        </div>
-        <CardTitle>Transactions</CardTitle>
-        <div className="text-xs text-muted-foreground pb-8">
-          Your latest recorded transactions
-        </div>
-        <div className="flex flex-col item-center justify-start">
-          <div className="w-24 ml-auto pr-4">
-            <PortfolioMenu />
-          </div>
-        </div>
-        <TransactionTable transactions={portfolio.transactions} />
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		if (portfolio && portfolio.networth !== undefined) {
+			setNetworth(portfolio.networth);
+		} else {
+			setNetworth(undefined);
+		}
+	}, [portfolio]);
+
+	console.log(portfolio);
+
+	if (loading) {
+		return <div>Loading...</div>;
+	}
+
+	if (userId === "") {
+		return <div>Please login to view your portfolio</div>;
+	}
+
+	return (
+		<div>
+			<div className="flex flex1 flex-col align-top">
+				<h1 className="text-4xl font-semibold w-auto text-left py-4 pl-6">
+					Portfolio
+				</h1>
+			</div>
+			<div>
+				<div className="flex flex-col items-center justify-center pb-16">
+					<Card className="items-center justify-center px-16 py-4">
+						<CardDescription>Your Net</CardDescription>
+						<CardTitle className="text-4xl">{networth}</CardTitle>
+						<CardTitle className="text-2xl">0.13 BTC</CardTitle>
+						<div className="text-xs text-muted-foreground">+25%</div>
+						<img src={plot} alt="plot" className="hidden" />
+					</Card>
+				</div>
+				<CardTitle>Transactions</CardTitle>
+				<div className="text-xs text-muted-foreground pb-8">
+					Your latest recorded transactions
+				</div>
+				<div className="flex flex-col item-center justify-start">
+					<div className="w-24 ml-auto pr-4">
+						<PortfolioMenu />
+					</div>
+				</div>
+				<TransactionTable transactions={portfolio.transactions} />
+			</div>
+		</div>
+	);
 }
 
 export default Portfolio;
--- src/TransactionTable.tsx
+++ src/TransactionTable.tsx
@@ -1,96 +1,110 @@
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
 } from "@/components/ui/table";
 
 const transaction_data = [
-  {
-    coin: "BTC",
-    type: "Buy",
-    date: "14-04-2024",
-    price: "61,879.00",
-    quantity: "0.01",
-    usdtValue: "+ $618.79",
-  },
-  {
-    coin: "BTC",
-    type: "Sell",
-    date: "14-04-2024",
-    price: "61,879.00",
-    quantity: "0.01",
-    usdtValue: "- $618.79",
-  },
-  {
-    coin: "BTC",
-    type: "Buy",
-    date: "14-04-2024",
-    price: "61,879.00",
-    quantity: "0.01",
-    usdtValue: "+ $618.79",
-  },
+	{
+		coin: "BTC",
+		type: "Buy",
+		date: "14-04-2024",
+		price: "61,879.00",
+		quantity: "0.01",
+		usdtValue: "+ $618.79",
+	},
+	{
+		coin: "BTC",
+		type: "Sell",
+		date: "14-04-2024",
+		price: "61,879.00",
+		quantity: "0.01",
+		usdtValue: "- $618.79",
+	},
+	{
+		coin: "BTC",
+		type: "Buy",
+		date: "14-04-2024",
+		price: "61,879.00",
+		quantity: "0.01",
+		usdtValue: "+ $618.79",
+	},
 ];
 
+// interface Transaction {
+// 	symbol: string;
+// 	type: "buy" | "sell";
+// 	date: string; // ISO date string
+// 	price: number;
+// 	quantity: number;
+// 	value: number;
+// }
+//
+// interface TransactionTableProps {
+// 	transactions: Transaction[];
+// }
+
+// export function TransactionTable({ transactions }: TransactionTableProps) {
 export function TransactionTable({ transactions }) {
-  console.log(transactions);
-  return (
-    <Card className="margin-0">
-      <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="sm:table-cell text-center">Coin</TableHead>
-              <TableHead className="sm:table-cell text-center">Type</TableHead>
-              <TableHead className="hidden md:table-cell text-center">
-                Date
-              </TableHead>
-              <TableHead className="hidden sm:table-cell text-center">
-                Price
-              </TableHead>
-              <TableHead className="sm:table-cell text-center">
-                Quantity
-              </TableHead>
-              <TableHead className="text-right">USDT Value</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {transactions.map((item, index) => (
-              <TableRow key={index}>
-                <TableCell>
-                  <div className="font-medium">{item.symbol}</div>
-                </TableCell>
-                <TableCell className="sm:table-cell">
-                  <Badge
-                    className="text-xs"
-                    variant={
-                      item.type.toLowerCase() === "buy"
-                        ? "default"
-                        : "secondary"
-                    }
-                  >
-                    {item.type}
-                  </Badge>
-                </TableCell>
-                <TableCell className="hidden md:table-cell">
-                  {item.date}
-                </TableCell>
-                <TableCell className="hidden sm:table-cell">
-                  {item.price}
-                </TableCell>
-                <TableCell className="sm:table-cell">{item.quantity}</TableCell>
-                <TableCell className="text-right">{item.value}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </CardContent>
-    </Card>
-  );
+	console.log(transactions);
+	return (
+		<Card className="margin-0">
+			<CardContent>
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead className="sm:table-cell text-center">Coin</TableHead>
+							<TableHead className="sm:table-cell text-center">Type</TableHead>
+							<TableHead className="hidden md:table-cell text-center">
+								Date
+							</TableHead>
+							<TableHead className="hidden sm:table-cell text-center">
+								Price
+							</TableHead>
+							<TableHead className="sm:table-cell text-center">
+								Quantity
+							</TableHead>
+							<TableHead className="text-right">USDT Value</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{transactions.map((item, index) => (
+							<TableRow key={index}>
+								<TableCell>
+									<div className="font-medium">{item.symbol}</div>
+								</TableCell>
+								<TableCell className="sm:table-cell">
+									<Badge
+										className="text-xs"
+										variant={
+											item.type.toLowerCase() === "buy"
+												? "default"
+												: "secondary"
+										}
+									>
+										{item.type}
+									</Badge>
+								</TableCell>
+								<TableCell className="hidden md:table-cell">
+									{item.date}
+								</TableCell>
+								<TableCell className="hidden sm:table-cell">
+									{item.price}
+								</TableCell>
+								<TableCell className="sm:table-cell">{item.quantity}</TableCell>
+								<TableCell className="text-right">{item.value}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</CardContent>
+		</Card>
+	);
 }
 
 export default TransactionTable;
